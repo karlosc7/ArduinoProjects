c4a69282603b25a259f4355ad922026643e4e8de
carlos <carlosc7fuen@gmail.com>
2018-10-15 13:42:54 +0200

    MiBlink
    ejemplo de voltaje o no voltaje en la placa Arduino UNO R3 Elegoo

---
+++ Blink/Blink.ino
@@ -0,0 +1,22 @@
+/*
+  Blink
+  This example code is in the public domain.
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+/*
+ * Carlos del Cerro Tenorio
+ */
+
+// reset board
+void setup() {
+  // inicializar LED_BUILTIN
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // voltaje
+  delay(500);                       // esperar medio segundo
+  digitalWrite(LED_BUILTIN, LOW);    // no voltaje
+  delay(500);                       // esperar medio segundo
+}
