a9d8e5ede9a4fd834d33705b6b9d35bb79c6bd15
carlos <carlosc7fuen@gmail.com>
2018-10-15 13:25:40 +0200

    Proyecto Semáforo
    Semáforo creado utilizando 3 leds y poniendo un delay para que cambie de posición

---
+++ semaforo/semaforo.ino
@@ -0,0 +1,35 @@
+/*************/
+/*  SEMAFORO */
+/*************/
+
+/*** Carlos del Cerro ***/
+
+//** Semaforo digital con 3 leds **//
+int rojo=2;      //definimos el valor del pin para el led rojo
+int amarillo=3;  //definimos el valor del pin para el led amarillo
+int verde=5;     //definimos el valor del pin para el led verde
+
+//** Programa **//
+
+void setup() {
+  pinMode(verde,OUTPUT);   //declaramos el pin verde como salida  
+  pinMode(amarillo,OUTPUT);//declaramos el pin amarillo como salida
+  pinMode(rojo,OUTPUT);    //declaramos el pin rojo como salida  
+}
+
+void loop() {
+ digitalWrite(verde,HIGH); //encendemos el led rojo
+ delay(2250);             //esperamos 2 segundos
+ digitalWrite(verde,LOW);  //apagamos el led rojo
+ delay(250);              //esperamos medio segundo
+ 
+ digitalWrite(amarillo,HIGH); //encendemos el led amarillo
+ delay(2250);                 //esperamos 2 segundos
+ digitalWrite(amarillo,LOW);  //apagamos el led amarillo
+ delay(250);                  //esperamos medio segundo
+ 
+ digitalWrite(rojo,HIGH); //encendemos el led verde
+ delay(2250);              //esperamos 2 segundos
+ digitalWrite(rojo,LOW);  //apagamos el led verde
+ delay(250);               //esperamos medio segundo
+}
