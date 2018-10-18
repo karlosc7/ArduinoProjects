93b6a5c238a6f9888403b5ec64e1ac005378f132
carlos <carlosc7fuen@gmail.com>
2018-10-18 16:09:30 +0200

    Placas solares
    Simulación de placas solares mediante dos fotodiodos y un servo.

---
+++ Placas_solares/Placas_solares.ino
@@ -0,0 +1,32 @@
+#include <Servo.h> //Cargamos la librería servo
+int valorLDR_derecha = 0;
+int valorLDR_izquierda = 0;
+int valor_Servo = 0;
+Servo miServo; //Creamos nuestro servo
+void setup(){
+Serial.begin(9600);
+miServo.attach(10);
+}
+void loop(){
+valorLDR_izquierda = analogRead(1);
+Serial.print("Sensor Izquierdo: ");
+Serial.print(valorLDR_izquierda);
+Serial.print(" ");
+valorLDR_derecha = analogRead(0);
+Serial.print("Sensor Derecho: ");
+Serial.print(valorLDR_derecha);
+Serial.print(" ");
+if (valorLDR_izquierda < valorLDR_derecha){
+valor_Servo = valor_Servo -10;
+if (valor_Servo < 0){ //El servo no admite valores negativos
+valor_Servo = 0; //así que los pasamos como 0
+}
+}
+else{
+valor_Servo = valor_Servo +10;
+if(valor_Servo > 179){
+valor_Servo = 179;
+}
+}
+miServo.write(valor_Servo);
+}
