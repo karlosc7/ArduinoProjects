e419355c3dff0a46f00ff21e7e31f6dc08f04bf9
carlos <carlosc7fuen@gmail.com>
2018-10-18 11:43:42 +0200

    Sistema de alarma
    Sistema de alarma mediante un sensor de infrarrojos, con un led y un buzzer

---
+++ Alarma/Alarma.ino
@@ -0,0 +1,22 @@
+const int analogInPin = A1;
+const int OutPin = 13;
+int sensorValue = 0;
+void setup() {
+// Inicializamos la comunicacion serial a 9600 bps:
+Serial.begin(9600);
+pinMode(OutPin, OUTPUT);
+}
+void loop() {
+sensorValue = analogRead(analogInPin);
+if(sensorValue > 900)
+{
+digitalWrite(OutPin, HIGH);
+}
+else
+{
+digitalWrite(OutPin, LOW);
+}
+Serial.print("sensor = " );
+Serial.println(sensorValue);
+delay(100);
+}
