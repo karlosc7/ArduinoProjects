2014114bc171ead992e46f38bce1499bfc360ee8
carlos <carlosc7fuen@gmail.com>
2018-10-19 09:50:18 +0200

    Sensor Temperatura
    Creación de un sensor de temperatura

---
+++ Sensor_Temperatura/Sensor_Temperatura.ino
@@ -0,0 +1,17 @@
+float temperatura = 0;
+int fan = 8;
+void setup(){
+Serial.begin (9600);
+pinMode(8,OUTPUT)
+}
+void loop(){
+//Calcula la temperatura usando como referencia 5v
+temperatura = (5.0 * analogRead(0)*100.0)/1023.0;
+Serial.println (temperatura);
+delay (500);
+if (temperatura < 20)
+{
+digitalWrite(8, LOW);
+} else
+digitalWrite(8,HIGH);
+}
